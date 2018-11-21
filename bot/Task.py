02f6cc3e971a8e07e6b97ec1d42a002a9cadd87a
LostInKadath <forgotten.in.kadath@gmail.com>
2018-11-21 22:42:21 +0300

    Added property Name

--- bot/Task.py
+++ bot/Task.py
@@ -1,10 +1,18 @@
+from constants import *
+
+
 class Task(object):
     """Description of any task"""
-    def __init__(self, linkToTask, linkToSolution, category, tags):
+    def __init__(self, name, linkToTask, linkToSolution, category, tags):
+        self.name = name
         self.linkToTask = linkToTask
         self.linkToSolution = linkToSolution
         self.category = category
         self.tags = sorted(tags)
+
+    @property
+    def Name(self):
+        return self.name
 
     @property
     def Problem(self):
@@ -17,4 +25,8 @@ class Task(object):
     @property
     def Category(self):
         return self.category
+
+
+def TaskDictionary():
+    return {str(i + 1) : Task(link, "", "", []) for i, link in enumerate(dict_of_tasks)}
 
